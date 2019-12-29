634990be26e15d0b41dd7726cb85b74c851113f8
Fabrizio Pani <fabek@gmx.co.uk>
2019-12-29 22:58:03 +0100

    fixed tcp port check, other improvements

--- net.sh
+++ net.sh
@@ -3,8 +3,27 @@
 
 is_valid_port(){
 
-:
-
+		 : 'BEGIN COMMENT
+	 """
+			Check if a valid TCP/UDP port
+			Args: port
+			Local vars: port
+			Global vars: err
+			Exit codes:
+				0: SUCCESS
+				1: GENERIC FAILURE
+	"""
+	END COMMENT'
+	local port=0
+	port=${1}
+	reset_global_vars
+	
+	if [[ $port -le 1 || $port -ge 65535 ]]; then
+		err_msg="Please insert a port in the range 1-65535"
+		err=1
+	fi	
+	
+	return $err
 }
 
 is_valid_ipv4(){
@@ -64,8 +83,7 @@ is_valid_ipv6(){
 	local ip="${1}"
 	str_REG="^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$"
 	
-	
-	
+		
 	get_bash_ver
 	
 	if [[ $major_ver -lt 3 ]]; then
@@ -73,8 +91,7 @@ is_valid_ipv6(){
 		err_msg="is_valid_ipv6: at least bash 3.0 is required for this check"
 		return $err
 	fi	
-	
-	
+		
 	if [[ $major_ver -ge 3 && $minor_ver -ge 2 ]]; then
 		if  ! [[ "$ip" =~ $str_REG ]]; then
 			err=1
@@ -92,8 +109,29 @@ is_valid_ipv6(){
 
 is_valid_host(){
 
-
-:
+	
+		 : 'BEGIN COMMENT
+	 """
+			Check if a valid host
+			Args: port
+			Local vars: host
+			Global vars: err
+			Exit codes:
+				0: SUCCESS
+				1: GENERIC FAILURE
+	"""
+	END COMMENT'
+	
+	local host=""
+	host=${1}
+	reset_global_vars
+	
+	if [[ -z $host  ]]; then
+		err_msg="Please insert a valid host"
+		err=1
+	fi	
+	
+	return $err
 
 
 }
@@ -154,7 +192,7 @@ check_tcp_port(){
 	 """
 			Check if a port is reachable
 			Args: host, port
-			Local vars: host, port, fd, max_fd, i
+			Local vars: host, port, fd, max_fd, i,ret
 			Global vars: set_val, err, err_msg
 			Exit codes:
 				0: SUCCESS
@@ -169,74 +207,96 @@ check_tcp_port(){
 
 	local host=${1} 
 	local port=${2}
-	#file descriptor
+	#file descriptor, values 0 1 2 are reserved
 	local fd=3
 	#system max file descriptor
 	local max_fd=0
-	local i=0
+	#loop variable
+	local ret=0
 
-	if ! `uname` != "Linux"; then
+	if ! echo "$(uname) "| grep -iq Linux; then
 		err=99 
 		err_msg="check_tcp_port: you are not using Linux"
 		return $err
 	fi	
 	if ! ls /proc/meminfo >/dev/null 2>&1; then
 	    err=99
-		err_msg="Linux : no proc file system"
+		err_msg="Linux : no proc file system??"
 		return $err
 	fi
+	#retrieving the maximum number of file descriptors
 	max_fd=$(cat /proc/sys/fs/file-max 2>/dev/null)
 	if  [[ $? -ne 0 ]]; then
-	    #cannot determine the max value of available 
-		#file descritprs
-		#hence setting a value of 1024
+	    #if we cannot determine the max number 
+		#of available file descriptors 
+		#we  set a value of 1024
 		max_fd=1024
 	fi
 	
-	
-	is_valid_host $host
-	if [[ $? -ne 0 ]]; then
-		echo "$err_msg"
-		return $err
-	fi
-	is_valid_port $port
-	if [[ $? -ne 0 ]]; then
-		echo "$err_msg"
-		return $err
-	fi
+	is_valid_host "$host"
+	 if [[ $? -ne 0 ]]; then
+		 return $err
+	 fi
+	 is_valid_port "$port"
+	 if [[ $? -ne 0 ]]; then
+			return $err
+	 fi
 
-	#verify or find free descriptor
-	while [ $ret -ne 0 ]; do
-	    ls /proc/$$/$(( fd + i)) >/dev/null 2>&1
+	#find a free available descriptor
+	while [ $ret -eq 0 ]; do
+	    ls /proc/$$/${fd} >/dev/null 2>&1
 	    ret=$?
-
-		if [[ $ret -ne 0 ]]; then
-		    i=$(( i+1))
-			fd=$(( fd+i))
+		if [[ $ret -eq 0 ]]; then
+			#file descriptor is in use, let's try the next 
+			fd=$(( fd+1 ))
 		fi
-		
 		if [[ $fd -ge $max_fd ]]; then
 		   err=100
 		   err_msg="check_tcp_port: fatal. No file descriptors available"
-		   return $err
+		fi
+	done
+	
+	#checking the TCP port
+	#u cannot normally pass a variable to exec,
+	#	u must pass a costant, hence using eval
+	if [[ $err -eq 0 ]]; then
+		eval 'exec '"$fd"'< "/dev/tcp/$host/$port"' 2>/dev/null
+		err=$?
+		if  ls   ls /proc/$$/${fd}  2>/dev/null ; then
+			exec ${fd}>&-
+		fi	
+	fi
+	#in case of issues we try to use nc or ncat  if available
+	
+	if [[ $err -ne 0 ]]; then
+	
+		check_net_tools	
+		
+		if [[ $is_nc -eq 0 ]]; then
+			if ! nc -uvz "$host" "$port" 2>/dev/null; then
+					err=1
+					err_msg="tcp port check failed"
+			else 
+					err=0
+					err_msg=""
+			fi	
+		return $err	
 		fi
 		
-	done
-		
-	if [[ $? -eq 0 ]]; then
-		#we choose the a value of descriptor
-		exec "${fd}"<>/dev/tcp/"${host}"/"${port}" 2>/dev/null 
-	    if [[ $? -ne 0 ]]; then
-			err=1
-			err_msg="Connection failure"
-	   fi
-	else
-		exec ${fd}>&-
-	fi
+		if [[ $is_ncat -eq 0 ]]; then
+			if ! ncat -uz "$host" "$port" 2>/dev/null; then 
+					err=1
+					err_msg="tcp port check failed"
+			else
+					err=0
+					err_msg=""
+			fi		
+	
+		return $err
+		fi
+	fi	
 
 	return $err
-
-
 
 }
 
@@ -263,14 +323,12 @@ check_udp_port(){
 	local host=${1} 
 	local port=${2}
 	
-	is_valid_host $host
-	if [[ $? -ne 0 ]]; then
-		echo "$err_msg"
+	 is_valid_host "$host"
+	 if [[ $? -ne 0 ]]; then
 		return $err
-	fi
-	is_valid_port $port
+	 fi
+	is_valid_port "$port"
 	if [[ $? -ne 0 ]]; then
-		echo "$err_msg"
 		return $err
 	fi
 		
@@ -279,21 +337,18 @@ check_udp_port(){
 		err_msg="check_udp_port: nc -z or ncat missing"
 		return $err
 	fi
-	
-
 	if [[ $is_nc -eq 0 ]]; then
-		nc -uz $host $port 2>/dev/null 
-			if [[ $? -ne 0 ]]; then
+		if ! nc -uvz "$host" "$port" 2>/dev/null; then
 				err=1
 				err_msg="udp port check failed"
-			fi	
-	else
-		ncat -uz $host $port 2>/dev/null 
-			if [[ $? -ne 0 ]]; then
+		fi	
+		return $err	
+	fi
+	if ! ncat -uz "$host" "$port" 2>/dev/null; then 
 				err=1
 				err_msg="udp port check failed"
-			fi	
 	fi		
+	
 	return $err
 
 }
