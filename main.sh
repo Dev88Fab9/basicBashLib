4a6445b875ead9adf0dc12ca29af52cc67fee230
Fabrizio Pani <fabek@gmx.co.uk>
2020-01-04 20:53:23 +0100

    adding a function to check for presence of essential tools

--- main.sh
+++ main.sh
@@ -70,3 +70,43 @@ minor_ver=$(bash --version|head -n1|awk -F"," '{print $2}'|awk  '{print $2}'|awk
 fix_ver=$(bash --version|head -n1|awk -F"," '{print $2}'|awk  '{print $2}'|awk -F"." '{print $3}'|awk -F"-"  '{print $1}')
 
 }
+
+
+chk_tools(){
+
+	 : 'BEGIN COMMENT
+	 """
+			check if essentials tools are in the system
+			and set relative global variable
+			Args: N/A, 
+			Local vars: N/A
+			Global vars: is_awk,is_grep,is_sed,is_tr,is_xargs
+			Exit codes: N/A
+	"""
+	END COMMENT'
+
+	is_grep=1
+	is_tr=1
+	is_sed=1
+	is_awk=1
+	is_xargs=1
+
+
+	if command -v grep >/dev/null 2>&1; then
+			is_grep=0
+	fi
+	if command -v tr >/dev/null 2>&1; then
+			is_tr=0
+	fi
+	if command -v sed >/dev/null 2>&1; then
+			is_sed=0
+	fi
+	if command -v awk >/dev/null 2>&1; then
+			is_awk=0
+	fi
+	if command -v xargs >/dev/null 2>&1; then
+			is_xargs=0
+	fi
+	
+	
+}
